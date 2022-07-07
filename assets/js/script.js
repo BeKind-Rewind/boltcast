5b1b4842f273db07293644192509b0490575fb19
Amanda Perry <challenge641@gmail.com>
2022-07-07 12:44:01 -0500

    added recent search list

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,45 @@
-// 5-day by city, state, country code:    api.openweathermap.org/data/2.5/forecast?q=" + city + "," + state + "," + country + "&units=imperial&appid=" + API_key
-// 5-day by zip code, country code:     api.openweathermap.org/data/2.5/forecast?zip=" + zip "," + country + "&units=imperial&appid=" + API_key
-// https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={API key}
-var searchCityFormEl = document.querySelector("#search-city-form");
+var searchCityFormEl = document.querySelector("#formSearch");
 var cityEl = document.querySelector("#cityInput");
-var dailyContainerEl = document.querySelector("#extendedForecast");
+var searchListEl = document.querySelector("#recentSearch");
 var todayForecastEl = document.querySelector("#rightNowStatus");
-var currentDateEl = document.querySelector("#currentDate")
+var currentDateEl = document.querySelector("#currentDate");
+var dailyContainerEl = document.querySelector("#extendedForecast");
+
 
 
 // This is "submit" event listening delegated to function "formSubmitHandler": "when button is clicked the response to the input is this:"
 var formSubmitHandler = function(event){
-    // upon event, this stops the browser from running default operations
     event.preventDefault();
-    // turn DOM value into var
+
+    var searchCityInput = document.querySelector("input[name='cityInput']").value;
+
+    // create list item
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "search-item";
+
+    // create div to hold search info and add to list item
+    var searchInfoEl = document.createElement("div");
+
+    // give it a class name
+    searchInfoEl.className = "search-info";
+
+    // add HTML content to div
+    searchInfoEl.innerHTML = "<h3 class='cityInput'>" + searchCityInput + "</h3>";
+    listItemEl.appendChild(searchInfoEl);
+
+    // add list item to list
+    searchListEl.appendChild(listItemEl);
+
     var cityInput = cityEl.value.trim();
     if (cityInput) {
         getForecast(cityInput);
-        // clear content
-        cityEl.value = "";
-        
+        // To clear the input textarea after search
+        // cityEl.value = "";   
     }
-
-    
-    
 };
+
+
+
 
 // function getForcast passing "city" will use the api call to query for the array of information associated with that "city" and DISPLAY it 
 var getForecast = function(city) {
@@ -36,7 +52,10 @@ var getForecast = function(city) {
         })
         .then(function(data){
             console.log(data);
-
+            var currentTime = new Date(data.dt * 1000);
+            var currentDate = document.getElementById("currentDate");
+            currentDate.textContent = data.name + " " + currentTime;
+            
             var lat = data.coord.lat
             var lon = data.coord.lon
 
@@ -55,7 +74,18 @@ var getForecast = function(city) {
                     var todaysUvi = document.getElementById("uvi");
                     todaysUvi.textContent = "UVIndex: " + data.current.humidity;
                     console.log(data, "second fetch");
+                    
+                    // 5 day forecast createElement cards from array?
+                    // var daily = document.getElementById("five-day-list-item");
+                    // var weatherCard = [
+                    //     new Date(data.daily[i].dt * 1000),
+                    //     document.getElementById('dailyIcon').src = `http://openweathermap.org/img/w/${data.daily.weather[0].icon}.png`,
+                    //     temp= data.daily[i].temp.day,
+                    //     wind= data.daily[i].wind_speed,
+                    //     humidity = data.daily[i].humidity,
+                    // ]
                 })
+                
         })
         .catch(function(error) {
             alert("Unable to connect to Weather");
